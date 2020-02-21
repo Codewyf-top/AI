6087f1cfad75f1e716c1e1473cf79ba7827890f7
codewyf <1090187390@qq.com>
2020-02-21 11:43:16 +0800

    2/21

--- Tensorflow/自动梯度.py
+++ Tensorflow/自动梯度.py
@@ -1,4 +1,16 @@
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2' #忽略CPU与tensorflow版本问题
 import tensorflow as tf
-
 #创建4个张量，并赋值
 a = tf.constant(1.)
+b = tf.constant(2.)
+c = tf.constant(3.)
+w = tf.constant(4.)
+
+with tf.GradientTape()as tape:#构建梯度环境
+    tape.watch([w])#将w加入梯度跟踪列表
+#构建计算过程，即函数表达式
+    y = a * w**2 + b * w +c
+#自动求导
+[dy_dw] = tape.gradient(y,[w])
+print(dy_dw)
